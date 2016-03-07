c750d869b644e4c0b92dc1a788f3bc7a5d7d3391
Paloma Venegas Stephens <palomavenegasstephens@Palomas-MacBook-Pro.local>
2016-03-07 10:42:10 -0600

    version before sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var watch = require('gulp-watch');
+var sass = require('gulp-sass');
 
-gulp.task('default', ['lint', 'watch']);
+gulp.task('default', ['lint', 'watch', 'styles']);
 
 gulp.task('watch', function() {
   gulp.watch('./javascripts/**/*.js', ['lint']);
@@ -13,4 +14,10 @@ gulp.task('lint', function() {
   return gulp.src('./javascripts/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
 });
